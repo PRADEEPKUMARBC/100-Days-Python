f0d595ddfd5fef2e604f961500024aabe64347de
PRADEEPKUMAR B C <pradeepkumarbc138@gmail.com>
2026-03-04 17:28:35 +0530

    Add files via upload

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("Welcome to Band name generator")
+Ask_city = input("What is your favorite city?\n")
+pet_name = input("What is your Favourite pet?\n")
+print("Your Band name could be " + Ask_city + " " + pet_name)
