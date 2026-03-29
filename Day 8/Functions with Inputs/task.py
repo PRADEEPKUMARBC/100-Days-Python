013cafc8a7445ce02b9937239514d8c4680bc76d
PRADEEPKUMAR B C <pradeepkumarbc138@gmail.com>
2026-03-29 21:15:37 +0530

    Add files via upload

---
+++ Day 8/Functions with Inputs/task.py
@@ -0,0 +1,6 @@
+def function_calling(name, age, height, weight):
+    print(f"Welcome to the Guess Game! {name} ")
+    print(f"Welcome to the Guess Game! {age} years old ")
+    print(f"Welcome to the Guess Game! {height} ")
+    print(f"Welcome to the Guess Game! {weight} ")
+function_calling("Pradeep", 20, 5.8, 58)
---
+++ Day 8/Positional vs Keyword Arguments/task.py
@@ -0,0 +1,10 @@
+def multiple_inputs(name, age, height, weight):
+    print(f"My name is {name} and I am {age} years old")
+    print(f"my height is {height} and my weight is {weight}")
+
+multiple_inputs("Pradeep", "20", "5.8","60")
+
+def greet_with(name="name", location="location"):
+    print(f"Hello {name}! I am {location}.")
+
+greet_with()
