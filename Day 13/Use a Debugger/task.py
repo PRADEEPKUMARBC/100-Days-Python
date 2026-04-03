61f609fd368aa169b22d2b7e3e097089b294d650
PRADEEPKUMAR B C <pradeepkumarbc138@gmail.com>
2026-04-03 18:14:14 +0530

    Add files via upload

---
+++ Day 13/Use a Debugger/task.py
@@ -0,0 +1,13 @@
+import random
+import maths
+
+def mutate_list(a_list):
+    b_list = []
+    new_item = 0
+    for item in a_list:
+        new_item = item * 2
+        new_item += random.randint(1, 3)
+        new_item = maths.add(new_item, item)
+    b_list.append(new_item)
+    print(b_list)
+mutate_list([1,3,4,6])
