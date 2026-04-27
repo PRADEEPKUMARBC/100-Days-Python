75713e5490aead155a7ff85cf479c362a2ce5f88
PRADEEPKUMAR B C <pradeepkumarbc138@gmail.com>
2026-04-27 19:32:30 +0530

    Add files via upload

---
+++ day-24/main.py
@@ -0,0 +1,9 @@
+# # with open("my_file.txt", mode='a') as file:
+# #     file.write("\n Pradeepkumar new line.")
+#
+# with open("C:\Users\USER\Downloads\turtle-crossing-start.zip", mode="w") as file:
+#     file.write("\n New line for example")
+
+txt = "I like banana"
+ans = txt.strip("banana", "pomogranate")
+print(ans)
