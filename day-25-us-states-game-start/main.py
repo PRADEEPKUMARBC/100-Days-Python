ba23256c713541e21ad55f8b7cb1d9e02d4681fa
PRADEEPKUMAR B C <pradeepkumarbc138@gmail.com>
2026-04-29 23:32:16 +0530

    Add files via upload

---
+++ day-25-us-states-game-start/main.py
@@ -0,0 +1,39 @@
+import turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S > States Game")
+
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}Guess the state", prompt="what's another state's name?").title()
+
+    if answer_state == "Exit":
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states")
+        break
+    # If answer_state is one of the states in all the states cof the 50_states.csv
+    if answer_state in all_states:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(state_data.x.item(), state_data.y.item())
+        t.write(state_data.state.item())
+        t.write(answer_state)
+        # create the turtle to write the name of the state at the  state's x and y coordinate
+
+screen.exitonclick()
